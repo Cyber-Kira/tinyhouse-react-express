1514134755869cad4a0aa1677ccfd22ae82e40b1
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-01-12 16:28:03 +0300

    add interfaces for database

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,4 +1,5 @@
 import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
 
 const user = "user_001";
 const userPassword = "4UUYQpXMapkBZ719";
@@ -6,7 +7,7 @@ const cluster = "cluster0.ku33n";
 
 const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url);
   const db = client.db("main");
 
