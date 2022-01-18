3b751704ac691cb44b78a64ef7f459be3db29e86
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-01-18 21:37:59 +0300

    feat: add bookings, listings, users collections

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-import { Database } from "../lib/types";
+import { Database, Booking, Listing, User } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 export const connectDatabase = async (): Promise<Database> => {
@@ -7,6 +7,8 @@ export const connectDatabase = async (): Promise<Database> => {
   const db = client.db("main");
 
   return {
-    listings: db.collection("test_listings"),
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
   };
 };
