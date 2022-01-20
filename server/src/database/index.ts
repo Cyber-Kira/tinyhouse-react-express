f0c05415cf64456dd548e2a73bdd8cfad8614249
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-01-20 10:14:54 +0300

    refactor: update codegen function

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -3,7 +3,7 @@ import { Database, Booking, Listing, User } from "../lib/types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 export const connectDatabase = async (): Promise<Database> => {
-  const client = await MongoClient.connect(url);
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
   const db = client.db("main");
 
   return {
